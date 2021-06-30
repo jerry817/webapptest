c3271910df00a71d2d5efa7885ae856875b9460b
Jerry Poon <jerry81711@gmail.com>
2021-06-30 14:44:27 +0100

    for proquest pre-screening test

--- src/com/proquest/interview/phonebook/Person.java
+++ src/com/proquest/interview/phonebook/Person.java
@@ -4,4 +4,43 @@ public class Person {
 	public String name;
 	public String phoneNumber;
 	public String address;
+
+	public Person(String name, String phoneNumber, String address) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.address = address;
+	}
+
+	@Override
+	public String toString() {
+		return "Person{" +
+				"name='" + name + '\'' +
+				", phoneNumber='" + phoneNumber + '\'' +
+				", address='" + address + '\'' +
+				'}';
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
 }
--- src/com/proquest/interview/phonebook/PhoneBookImpl.java
+++ src/com/proquest/interview/phonebook/PhoneBookImpl.java
@@ -1,31 +1,82 @@
 package com.proquest.interview.phonebook;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.proquest.interview.util.DatabaseUtil;
 
 public class PhoneBookImpl implements PhoneBook {
-	public List people;
-	
+	private List<Person> people = new ArrayList<>();
+
 	@Override
 	public void addPerson(Person newPerson) {
 		//TODO: write this method
+		if(newPerson != null){
+			people.add(newPerson);
+		}
 	}
-	
+
 	@Override
-	public Person findPerson() {
+	public Person findPerson(String firstName, String lastName) {
 		//TODO: write this method
+		String name = null;
+		if(firstName != null && !firstName.isEmpty()){
+			name = firstName;
+		}
+		if(lastName != null && !lastName.isEmpty()){
+			name = (name == null) ? lastName : name + " " + lastName;
+		}
+		for(Person p : people){
+			if(p.getName().equals(name)){
+				return p;
+			}
+		}
+		return null;
 	}
-	
+
+	public List<Person> getPeople() {
+		return people;
+	}
+
 	public static void main(String[] args) {
 		DatabaseUtil.initDB();  //You should not remove this line, it creates the in-memory database
 
 		/* TODO: create person objects and put them in the PhoneBook and database
 		 * John Smith, (248) 123-4567, 1234 Sand Hill Dr, Royal Oak, MI
 		 * Cynthia Smith, (824) 128-8758, 875 Main St, Ann Arbor, MI
-		*/ 
+		*/
+		Person john = new Person("John Smith", "(248) 123-4567", "Royal Oak, MI");
+		Person cynthia = new Person("Cynthia Smith", "(824) 128-8758", "875 Main St, Ann Arbor, MI");
+		PhoneBookImpl phoneBook = new PhoneBookImpl();
+		phoneBook.addPerson(john);
+		phoneBook.addPerson(cynthia);
+
 		// TODO: print the phone book out to System.out
+		for(Person p : phoneBook.getPeople()){
+			System.out.println(p);
+		}
+
 		// TODO: find Cynthia Smith and print out just her entry
+		System.out.println(phoneBook.findPerson("Cynthia", "Smith"));
+
 		// TODO: insert the new person objects into the database
+		try {
+			Connection cn = DatabaseUtil.getConnection();
+			for (Person person : phoneBook.getPeople()) {
+				PreparedStatement stmt = cn.prepareStatement(
+						"INSERT INTO PHONEBOOK (NAME, PHONENUMBER, ADDRESS) values (?,?,?)");
+				stmt.setString(1, person.getName());
+				stmt.setString(2, person.getPhoneNumber());
+				stmt.setString(3, person.getAddress());
+				stmt.execute();
+			}
+			cn.commit();
+			cn.close();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+		}
 	}
 }
